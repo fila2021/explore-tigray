7be58cc7ce8da142286c14265739e3466c23daf3
fila2021 <timtey2015@gmail.com>
2025-09-21 23:41:43 +0100

    Add destination data in main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const destinations = [
+  {
+    name: "Axum",
+    description: "Historic city with ancient obelisks.",
+    image: "images/axum.jpg",
+  },
+  {
+    name: "Gera'alta",
+    description: "Rock-hewn churches and mountains.",
+    image: "images/geraalta.jpg",
+  },
+  {
+    name: "Yeha",
+    description: "Ancient temples and archaeology.",
+    image: "images/yeha.jpg",
+  },
+];
