5f91c429b319db96193be4ace793c68ff78eb101
fila2021 <timtey2015@gmail.com>
2025-09-22 01:34:23 +0100

    Deploy full Explore Tigray project

--- js/map.js
+++ js/map.js
@@ -6,12 +6,11 @@ function initMap() {
     zoom: 8,
     center: { lat: 14.3, lng: 39.0 },
   });
-
-  updateMapMarkers(destinations);
+  if (typeof updateMapMarkers === "function") updateMapMarkers(destinations);
 }
 
 function updateMapMarkers(list) {
-  markers.forEach((marker) => marker.setMap(null));
+  markers.forEach((m) => m.setMap(null));
   markers = [];
 
   list.forEach((dest) => {
@@ -20,12 +19,10 @@ function updateMapMarkers(list) {
       map: map,
       title: dest.name,
     });
-
-    const infoWindow = new google.maps.InfoWindow({
+    const info = new google.maps.InfoWindow({
       content: `<h5>${dest.name}</h5><p>${dest.description}</p>`,
     });
-
-    marker.addListener("click", () => infoWindow.open(map, marker));
+    marker.addListener("click", () => info.open(map, marker));
     markers.push(marker);
   });
 }
